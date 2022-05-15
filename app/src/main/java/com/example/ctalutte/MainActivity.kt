55b898d9d5b54a9748de3248413c557d0812b0bb
ArnoVoxel <jimmymoliere@gmail.com>
2022-05-15 10:59:27 +0200

    ajout bonhomme animé avec exemple d'intégration dans le activity_main.xml

--- app/src/main/java/com/example/ctalutte/MainActivity.kt
+++ app/src/main/java/com/example/ctalutte/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.ctalutte
 
 import androidx.appcompat.app.AppCompatActivity
+import android.graphics.drawable.Animatable
+import android.graphics.drawable.Animatable2
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {

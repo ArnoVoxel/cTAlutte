fc43b1799d4f1f634b66b3188c89dde4b995aeaf
Ngervaux <n.gervaux@gmail.com>
2022-05-19 15:51:47 +0200

    Tract : ecran ok / Deplacement tract ok

--- app/src/main/java/com/example/ctalutte/MainActivity.kt
+++ app/src/main/java/com/example/ctalutte/MainActivity.kt
@@ -17,42 +17,68 @@ import android.widget.ImageView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.DialogFragment
 import android.app.FragmentManager
+import android.content.Context
+import android.content.Intent
+import android.widget.Button
+import androidx.appcompat.app.AlertDialog
 
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var startMenuLayout: View
+
+    fun validerAdhesion(){
+
+
+            startMenuLayout = findViewById(R.id.modal_accueil)
+            startMenuLayout.visibility = View.GONE
+            // abonnement du bouton pour lancer la musique
+            var boutonRadio = findViewById<ImageButton>(androidx.appcompat.R.id.radio);
+            boutonRadio.setOnClickListener(View.OnClickListener {
+                var chanson = MediaPlayer.create(this, R.raw.international_fr)
+                chanson?.start()
+                boutonRadio.setOnClickListener(null)
+            })
+
+            //abonnement bouton dossier
+        var boutonDossier = findViewById<ImageButton>(R.id.bouton_dossier)
+        boutonDossier.setOnClickListener(View.OnClickListener {
+            Outils.toastCourt(this, "Tu vas bosser fidèle Camarade")
+            val monIntent : Intent = Intent(this,TractActivity::class.java)
+            startActivity(monIntent)
+        })
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         //Affichage en plein écran + suprresion barre status
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-            window.insetsController?.hide(WindowInsets.Type.statusBars())
-        }
-        else {
+        Outils.logPerso("PLOP","Avant hide window")
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+//            Outils.logPerso("PLOP","Entrée if")
+//            window.insetsController?.hide(WindowInsets.Type.statusBars())
+//        }
+//        else {
+            Outils.logPerso("PLOP","Entrée Else")
             @Suppress("DEPRECATION")
             window.setFlags(
                 WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN
             )
-        }
+//        }
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //Création de la modale
-//        setContentView(R.layout.menu_fragment)
- /*       Log.d("PLOP","Avant crea vue")
 
-        Log.d("PLOP","Apres crea vue")
-        val accueilFragment:AccueilFragment= AccueilFragment.getInstance()
-        Log.d("PLOP","Apres getinstance")
-        accueilFragment.show(supportFragmentManager,AccueilFragment.TAG)
-        Log.d("PLOP","Apres show")*/
-
-
-
-        var boutonRadio = findViewById<ImageButton>(androidx.appcompat.R.id.radio);
-        boutonRadio.setOnClickListener(View.OnClickListener {
-            var chanson = MediaPlayer.create(this, R.raw.the_internationale_english)
-            chanson?.start()
+        var boutonAdhesion = findViewById<Button>(R.id.bouton_valider_modal)
+        boutonAdhesion.setOnClickListener(View.OnClickListener {
+            validerAdhesion()
         })
+        //Création de la modale
+
+        /*var popup = StartMenuKotlin()
+        popup.show(supportFragmentManager, "popo")*/
+
+
+
 
     }
 
---
+++ app/src/main/java/com/example/ctalutte/TractActivity.kt
@@ -0,0 +1,42 @@
+package com.example.ctalutte
+
+import android.content.ClipData
+import android.content.ClipDescription
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.view.DragEvent
+import android.view.WindowManager
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+
+class TractActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        @Suppress("DEPRECATION")
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_distrib)
+        var tract = findViewById<ImageView>(R.id.drop_tract)
+        tract.apply {
+            setOnLongClickListener { view ->
+                val item = ClipData.Item(view.tag as? CharSequence)
+
+                val dragData = ClipData(
+                    view.tag as? CharSequence,
+                    arrayOf(ClipDescription.MIMETYPE_TEXT_PLAIN),item)
+
+                val myShadow = MyDragShadowBuilder(this)
+                view.startDragAndDrop(dragData,
+                                    myShadow,
+                                    null,
+                                                0)
+                true
+            }
+        }
+
+    }
+}

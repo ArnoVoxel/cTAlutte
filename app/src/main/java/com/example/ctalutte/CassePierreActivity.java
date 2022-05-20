41eb043cfeabc2e3034b8f8044aa4d78338e84f4
ArnoVoxel <jimmymoliere@gmail.com>
2022-05-20 10:28:24 +0200

    tremblement de pierre et zone bouton transparent

--- app/src/main/java/com/example/ctalutte/CassePierreActivity.java
+++ app/src/main/java/com/example/ctalutte/CassePierreActivity.java
@@ -2,32 +2,21 @@ package com.example.ctalutte;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.animation.ObjectAnimator;
-import android.content.Context;
-import android.graphics.Bitmap;
+import android.animation.PropertyValuesHolder;
+import android.animation.ValueAnimator;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.os.CountDownTimer;
-import android.text.Layout;
-import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageButton;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
-
 import com.github.jinatonic.confetti.CommonConfetti;
-import com.github.jinatonic.confetti.ConfettiManager;
-import com.github.jinatonic.confetti.ConfettiSource;
-import com.github.jinatonic.confetti.ConfettiView;
-import com.github.jinatonic.confetti.ConfettoGenerator;
-import com.github.jinatonic.confetti.Utils;
-import com.github.jinatonic.confetti.confetto.BitmapConfetto;
-import com.github.jinatonic.confetti.confetto.Confetto;
+import androidx.dynamicanimation.animation.DynamicAnimation;
+import androidx.dynamicanimation.animation.SpringAnimation;
+import androidx.dynamicanimation.animation.SpringForce;
 
 import java.lang.reflect.Modifier;
 import java.text.AttributedCharacterIterator;
@@ -43,13 +32,23 @@ public class CassePierreActivity extends AppCompatActivity {
     public int decompte = 10;
 
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_casse_pierre);
 
+        //animation de la pierre
+        View pierreShake = findViewById(R.id.image_pierre);
+        final SpringAnimation animation = new SpringAnimation(pierreShake, SpringAnimation.ROTATION, 5f);
+        animation.getSpring().setDampingRatio(SpringForce.DAMPING_RATIO_HIGH_BOUNCY);
+        animation.addEndListener(new DynamicAnimation.OnAnimationEndListener() {
+            @Override
+            public void onAnimationEnd(DynamicAnimation animation, boolean canceled, float value, float velocity) {
+                pierreShake.setRotation(0f);
+                animation.cancel();
+            }
+        });
 
         //position du touch pour particules
         View pierreEcran = findViewById(R.id.pierre_ecran);
@@ -79,13 +78,15 @@ public class CassePierreActivity extends AppCompatActivity {
         //intégration du score
         TextView scoreJoueur = (TextView)findViewById(R.id.score_joueur);
         scoreJoueur.setText("0");
-        ImageButton boutonPierre = (ImageButton) findViewById(R.id.grosse_pierre);
+        Button boutonPierre = (Button) findViewById(R.id.grosse_pierre);
         boutonPierre.setOnClickListener(new View.OnClickListener() {
+            //initiliasation des variables
             int score = 0;
             int total = Integer.valueOf(getString(R.string.score));
 
             @Override
             public void onClick(View v) {
+                animation.cancel();
 
                 if(score == total){
                     Outils.toastCourt(getApplicationContext(), "RALACHO TAVARICH !");
@@ -96,11 +97,15 @@ public class CassePierreActivity extends AppCompatActivity {
                     TextView scoreJoueur = (TextView)findViewById(R.id.score_joueur);
                     scoreJoueur.setText(Integer.toString(score));
 
-                    ViewGroup layoutPierres = (ViewGroup)findViewById(R.id.layout_pierres).getParent();
+
+                    animation.start();
+
 
                     //particules
+                    ViewGroup layoutPierres = (ViewGroup)findViewById(R.id.layout_pierres).getParent();
                     CommonConfetti.rainingConfetti(layoutPierres, new int[] {Color.BLACK}).oneShot();
                     //CommonConfetti.explosion(layoutPierres, 1000, 1000, new int[] {Color.BLACK});
+
                     Outils.logPerso("Pierre", "touch position X "+ MotionEvent.AXIS_X);
                     Outils.logPerso("Pierre", "touch position Y "+ valeurY[0]);
 

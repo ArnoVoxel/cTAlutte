6141cd116561728d1260223ede1823b0616168e2
ArnoVoxel <jimmymoliere@gmail.com>
2022-05-20 15:36:56 +0200

    création d'une BDD avec enregistrement du nom

---
+++ app/src/main/java/com/example/ctalutte/Connexion.java
@@ -0,0 +1,64 @@
+package com.example.ctalutte;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import androidx.annotation.Nullable;
+
+public class Connexion extends SQLiteOpenHelper {
+
+    private static final String DB_NAME = "lutte";
+    private static final Integer DB_VERSION = 1;
+    private static final String TABLE_NAME = "camarades";
+    private static final String NOM_JOUEUR = "nom_joueur";
+    private static final String SCORE = "score";
+    private static final String NB_TACHES = "nb_taches";
+
+    public Connexion(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version ) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+        String query = "CREATE TABLE " +TABLE_NAME+" ("
+                +NOM_JOUEUR+" TEXT PRIMARY KEY, "
+                +SCORE+" int NOT NULL, "
+                +NB_TACHES+" int NOT NULL )";
+
+        db.execSQL(query);
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE "+ TABLE_NAME);
+        onCreate(db);
+    }
+
+    public SQLiteDatabase getReadableDatabase(){
+        return super.getReadableDatabase();
+    }
+
+    public SQLiteDatabase getWritableDatabase(){
+        return super.getWritableDatabase();
+    }
+
+    public void ajouterCamarade(String nomCmarade, Integer scoreCamarade, Integer nbTachesCamarade){
+        Outils.logPerso("BDD","début fct ajouter");
+        SQLiteDatabase db = getReadableDatabase();
+        Outils.logPerso("BDD","après getReadable");
+        scoreCamarade = 0;
+        nbTachesCamarade = 0;
+
+        String insertCamarade ="INSERT INTO "+TABLE_NAME+" ("+NOM_JOUEUR+", "+SCORE+", "+NB_TACHES+") \n VALUES('"+nomCmarade+"', "+scoreCamarade+", "+nbTachesCamarade+");";
+        Outils.logPerso("BDD","après création de la string de requête");
+        Outils.logPerso("BDD","requête : "+insertCamarade);
+
+        db.execSQL(insertCamarade);
+        Outils.logPerso("BDD","après insert en BDD");
+        db.close();
+    }
+}
--- app/src/main/java/com/example/ctalutte/MainActivity.kt
+++ app/src/main/java/com/example/ctalutte/MainActivity.kt
@@ -12,31 +12,42 @@ import android.util.Log
 import android.view.View
 import android.view.WindowInsets
 import android.view.WindowManager
-import android.widget.ImageButton
-import android.widget.ImageView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.DialogFragment
 import android.app.FragmentManager
 import android.content.Context
 import android.content.Intent
-import android.widget.Button
+import android.provider.BaseColumns
+import android.widget.*
 import androidx.appcompat.app.AlertDialog
 
 
 class MainActivity : AppCompatActivity() {
+
+    val DB_NAME = "lutte";
+    val DB_VERSION = 1;
+
     private lateinit var startMenuLayout: View
 
     fun validerAdhesion(){
 
+        var nomCamarade = findViewById<EditText>(R.id.champ_nom_utilisateur)
+        Outils.logPerso("BDD", "après récupération du nom"+nomCamarade.text.toString())
+        //nomCamarade.text.toString()
+        val connexionBDD = Connexion(this, DB_NAME,null,DB_VERSION)
+        Outils.logPerso("BDD", "après création connexion")
+        connexionBDD.ajouterCamarade(nomCamarade.text.toString(), 0, 0)
+        Outils.logPerso("BDD", "après insert")
 
-            startMenuLayout = findViewById(R.id.modal_accueil)
-            startMenuLayout.visibility = View.GONE
-            // abonnement du bouton pour lancer la musique
-            var boutonRadio = findViewById<ImageButton>(androidx.appcompat.R.id.radio);
-            boutonRadio.setOnClickListener(View.OnClickListener {
-                var chanson = MediaPlayer.create(this, R.raw.the_internationale_english)
-                chanson?.start()
-            })
+        startMenuLayout = findViewById(R.id.modal_accueil)
+        startMenuLayout.visibility = View.GONE
+
+        // abonnement du bouton pour lancer la musique
+        var boutonRadio = findViewById<ImageButton>(androidx.appcompat.R.id.radio);
+        boutonRadio.setOnClickListener(View.OnClickListener {
+            var chanson = MediaPlayer.create(this, R.raw.the_internationale_english)
+            chanson?.start()
+        })
 
             //abonnement bouton dossier
         var boutonDossier = findViewById<ImageButton>(R.id.bouton_dossier)

5577322f2b51666a1071f0e9549ce58d7644bf39
Ngervaux <n.gervaux@gmail.com>
2022-06-08 13:31:29 +0200

    Réglage activitée Tract animation ok gestion poubelle ok

--- app/src/main/java/com/example/ctalutte/Modele/FragmentController.kt
+++ app/src/main/java/com/example/ctalutte/Modele/FragmentController.kt
@@ -5,6 +5,7 @@ import com.example.ctalutte.Outils
 
 import android.app.Activity
 import android.content.Context
+import android.content.IntentSender
 import android.os.CountDownTimer
 import android.text.Layout
 import android.widget.ImageView
@@ -25,21 +26,15 @@ import com.example.ctalutte.R
 fun timer(): CountDownTimer {
 //var compteur = getCompteur()
  compteur = object : CountDownTimer(decompte*1000, 1000) {
-  override fun onTick(millisUntilFinished: Long) {
-   chronoTache?.setText(decompte.toString())
-
-      decompte--
-
-  }
-
-  override fun onFinish() {
-
-      Outils.toastCourt(context, "Au GOULAG !")
-
-//      getActivity().finish()
-      activity?.finish()
-  }
- }.start()
+      override fun onTick(millisUntilFinished: Long) {
+           chronoTache?.setText(decompte.toString())
+           decompte--
+      }
+      override fun onFinish() {
+          Outils.toastCourt(context, "Au GOULAG !")
+          activity?.finish()
+      }
+     }.start()
     return compteur as CountDownTimer
 }
 
